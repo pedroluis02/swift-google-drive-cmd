1086073347b45284bc55bf18310ca68bfc871c7d
Pedro Luis <pedroluisnr22@gmail.com>
2024-09-11 23:20:45 -0500

    build: :wrench: name  executable gdrive-cli

--- Package.swift
+++ Package.swift
@@ -4,9 +4,6 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftGoogleDriveCmd",
-    dependencies: [
-        .package(url: "https://github.com/darrarski/swift-google-drive-client.git", from: "0.12.1")
-    ],
     targets: [
         .executableTarget(name: "gdrive-cli")
     ]
