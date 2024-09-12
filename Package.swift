5fda3d39ae94ecdaac6a879d89954172430dabae
Pedro Luis <pedroluisnr22@gmail.com>
2024-09-11 23:55:01 -0500

    build: :package: add argument-parser package

--- Package.swift
+++ Package.swift
@@ -4,7 +4,15 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftGoogleDriveCmd",
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.3.0")
+    ],
     targets: [
-        .executableTarget(name: "gdrive-cli")
+        .executableTarget(
+            name: "gdrive-cli",
+            dependencies: [
+                .product(name: "ArgumentParser", package: "swift-argument-parser")
+            ]
+        )
     ]
 )
