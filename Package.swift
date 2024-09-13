72dddd578676e2531f9546249e72ef4309f81124
Pedro Luis <pedroluisnr22@gmail.com>
2024-09-12 20:09:49 -0500

    build: :package: add google-drive-client package

--- Package.swift
+++ Package.swift
@@ -4,14 +4,19 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftGoogleDriveCmd",
+    platforms: [
+        .macOS(.v13)
+    ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.3.0")
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.3.0"),
+        .package(url: "https://github.com/darrarski/swift-google-drive-client", from: "0.12.1")
     ],
     targets: [
         .executableTarget(
             name: "gdrive-cli",
             dependencies: [
-                .product(name: "ArgumentParser", package: "swift-argument-parser")
+                .product(name: "ArgumentParser", package: "swift-argument-parser"),
+                .product(name: "GoogleDriveClient", package: "swift-google-drive-client")
             ]
         )
     ]
