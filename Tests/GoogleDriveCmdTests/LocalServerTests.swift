d386e9d25fe32943cf4169c3a24f64b294267787
Pedro Luis <pedroluisnr22@gmail.com>
2024-09-15 22:21:17 -0500

    test: :white_check_mark: add server test

---
+++ Tests/GoogleDriveCmdTests/LocalServerTests.swift
@@ -0,0 +1,37 @@
+import XCTest
+import Foundation
+import NIOHTTP1
+@testable import gdrive_cli
+
+final class LocalServerTests: XCTestCase {
+    func testStartServer() throws {
+        let host = "localhost"
+        let port = 8080
+        let urlString = "http://\(host):\(port)"
+        
+        let server = HttpServer(host: host, port: port)
+        
+        let requestResult = expectation(description: "Request call result")
+        var responseStatus: Int? = nil
+        DispatchQueue.global().asyncAfter(deadline: .now() + 0.2) {
+            let url = URL(string: urlString)!
+            URLSession.shared.dataTask(with: url) { (_, response, _) in
+                if let httpResponse = response as? HTTPURLResponse {
+                    responseStatus = httpResponse.statusCode
+                }
+                requestResult.fulfill()
+            }.resume()
+        }
+        
+        try startAndListenServerASync(with: server)
+        
+        wait(for: [requestResult], timeout: 10.0)
+        XCTAssertNotNil(responseStatus)
+    }
+    
+    private func startAndListenServerASync(with server: HttpServer) throws {
+        try server.start() { server, request -> (String, HTTPResponseStatus) in
+            return ("OK", .ok)
+        }
+    }
+}
